bc998bb8440828c276b23ab3f6fdb105856d2249
galvanix <elia@dl1ye.com>
2018-07-12 09:20:48 +0200

    got basic socks5 proxy functionality working

--- ptpy.py
+++ ptpy.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-import json,subprocess
+import json,subprocess,sys
 
-with open("env.json") as f:
+with open(sys.argv[1]) as f:
     env_vars = json.loads(f.read())
 
+
+proc = subprocess.Popen(["/usr/bin/obfs4proxy","-enableLogging","-logLevel=DEBUG"],
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        env=env_vars)
+
+print(f"Started obfs4proxy with PID {proc.pid}")
+
+try:
+    for m in proc.stdout:
+        print(m.decode())
+
+    print(proc.returncode)
+
+except KeyboardInterrupt:
+    proc.terminate()
