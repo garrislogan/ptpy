a51067c8f23747605b48de5652bfb73a61acef98
galvanix <elia@dl1ye.com>
2018-07-13 09:16:25 +0200

    changed string formatting to ensure compatibility with Python3 version < 3.6

--- ptpy.py
+++ ptpy.py
@@ -5,19 +5,20 @@ import json,subprocess,sys
 with open(sys.argv[1]) as f:
     env_vars = json.loads(f.read())
 
-
 proc = subprocess.Popen(["/usr/bin/obfs4proxy","-enableLogging","-logLevel=DEBUG"],
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         env=env_vars)
 
-print(f"Started obfs4proxy with PID {proc.pid}")
+print("Started obfs4proxy with PID {}".format(proc.pid))
 
 try:
     for m in proc.stdout:
         print(m.decode())
 
-    print(proc.returncode)
+
+
+#    print(proc.returncode)
 
 except KeyboardInterrupt:
     proc.terminate()
