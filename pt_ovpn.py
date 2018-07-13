4393d1fa2467841af895f9c5e09614642fe8572c
galvanix <elia@dl1ye.com>
2018-07-13 16:37:07 +0200

    improved config file situation, PT+OpenVPN launcher uses one unfied config file to start the pluggable transport proxy and openvpn

--- pt_ovpn.py
+++ pt_ovpn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import json,subprocess,sys
+import subprocess,sys,configparser
 
 def pt_startup_wait(proc):
     """
@@ -10,11 +10,11 @@ def pt_startup_wait(proc):
     """
     pt_state = {"methods": []} 
     
-    for m in stdout.decode():
-        fs = m.split(" ")
-            
+    for m in proc.stdout:
+        fs = m.decode().strip().split(" ")
+ 
         if fs[0].endswith("ERROR"):
-            raise RuntimeError("Pluggable transport: " + m) 
+            raise RuntimeError("Pluggable transport: " + m.decode().strip()) 
 
         elif fs[0] == "CMETHOD":
             ip,port = fs[3].split(":")
@@ -23,15 +23,15 @@ def pt_startup_wait(proc):
                                         "proto": fs[2],
                                         "addr": (ip,int(port))})
 
-        elif fs[0] == "CMETHODS" and fields[1] == "DONE":
+        elif fs[0] == "CMETHODS" and fs[1] == "DONE":
             pt_state["mode"] = "client"
             break       
  
         elif fs[0] == "SMETHOD":
             ip,port = fs[2].split(":")
 
-            method["transport"] = fs[1]
-            method["addr"] = (ip,int(port)) 
+            method = {"transport": fs[1],
+                      "addr": (ip,int(port))}
 
             try:
                 method["args"] = fs[3][5:]
@@ -40,45 +40,51 @@ def pt_startup_wait(proc):
 
             pt_state["methods"].append(method)
 
-        elif fs[0] == "SMETHODS" and fields[1] == "DONE":
+        elif fs[0] == "SMETHODS" and fs[1] == "DONE":
             pt_state["mode"] = "server"
             break
 
     return pt_state
 
-def start_pt(pt_env_fname):
+def start_pt(conf):
     """
-    Start the PT using the environment specified in the pt_env_filename file
+    Start the PT according to the options from the PT config section 
     """
-    with open(pt_env_fname) as f:
-        pt_env = json.loads(f.read()) 
+    env = {}
+    env["TOR_PT_MANAGED_TRANSPORT_VER"] = conf["transport_version"]
+    env["TOR_PT_STATE_LOCATION"] = conf["state_dir"]
+    env["TOR_PT_EXIT_ON_STDIN_CLOSE"] = "0" 
+   
+    if conf["type"] == "client":
+        env["TOR_PT_CLIENT_TRANSPORTS"] = conf["transports"] 
 
-    pt_proc = subprocess.Popen(["/usr/bin/obfs4proxy","-enableLogging","-logLevel=DEBUG"],
+    elif conf["type"] == "server":
+        env["TOR_PT_SERVER_TRANSPORTS"] = conf["transports"]
+        env["TOR_PT_SERVER_BINDADDR"] = conf["bindaddr"] 
+    
+        if "orport" in conf:
+            env["TOR_PT_ORPORT"] = conf["orport"]
+
+        if "extorport" in conf:
+            env["TOR_PT_EXTORPORT"] = conf["extorport"]
+
+    pt_proc = subprocess.Popen(conf["exec"].split(" "),
                             stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE,
-                            env=pt_env)
+                            env=env)
 
     print("Started obfs4proxy with PID",pt_proc.pid)
 
     pt_state = pt_startup_wait(pt_proc)   
 
     return pt_proc,pt_state
- 
-if __name__ == "__main__":
 
-    # config file names
-    pt_env_fname = sys.argv[1]
-    ovpn_conf_fname = sys.argv[2]
-    ovpn_auth_fname = sys.argv[3]
-
-    # start pluggable transport
-    pt_proc, pt_state = start_pt(pt_env_fname)
- 
-    # start OpenVPN
-    
+def start_ovpn(conf,pt_state):
     # all OpenVPN options and configuration except the SOCKS5 proxy
     # should be configured using a regular OpenVPN config file
-    confarg = "--config {}".format(ovpn_conf_name)
+
+    proc_cmd = [conf["exec"],
+                "--config {}".format(conf["config"])]
     
     if pt_state["mode"] == "client":
         # find first available SOCKS5 proxy provided by pluggable transport
@@ -88,18 +94,27 @@ if __name__ == "__main__":
             raise RuntimeError("No suitable SOCKS5 proxy available to OpenVPN")
        
         # OpenVPN command line option to use the SOCKS5 proxy provided by PT
-        socksarg = "--socks-proxy {} {} {}".format(s5p["addr"][0],
-                                                   s5p["addr"][1],
-                                                   ovpn_auth_fname)
+        proc_cmd.append("--socks-proxy {} {} {}".format(s5p["addr"][0],
+                                                        s5p["addr"][1],
+                                                        conf["socks_auth"]))
 
-        ovpn_proc = subprocess.Popen(["/usr/sbin/openvpn",socksarg,confarg])
-
-    elif pt_state["mode"] == "server": 
-        print(pt_state["methods"]
-        ovpn_proc = subprocess.Popen(["/usr/sbin/openvpn",confarg])
+    ovpn_proc = subprocess.Popen(proc_cmd)
 
     print("Started OpenVPN with PID",ovpn_proc.pid)
 
+    return ovpn_proc
+
+if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+
+    # start pluggable transport
+    pt_proc, pt_state = start_pt(config["PT"])
+
+    # start OpenVPN
+    ovpn_proc = start_ovpn(config["OpenVPN"],pt_state)   
+ 
     while ovpn_proc.poll() == None or pt_proc.poll() == None:
         pass
 
